41a6071d43353b5ae80784e5a1d4b71efb7b05a6
Róger Valderrama <roger.andres18@gmail.com>
2018-05-01 19:43:21 -0600

    Ejemplo

--- src/main/java/view/LoginController.java
+++ src/main/java/view/LoginController.java
@@ -8,7 +8,7 @@ import javafx.fxml.FXML;
 public class LoginController {
 
     @FXML
-    private JFXPasswordField PasswordField;
+    private JFXPasswordField passwordField;
 
     @FXML
     private JFXButton loginBtn;
@@ -16,4 +16,14 @@ public class LoginController {
     @FXML
     private JFXTextField userField;
 
+    /**
+     * Extrae texto que uno escribe en el user y la pone en password y al mismo tiempo desactiva el boton de login. En el FXML esta para que cuando se de click al boton de login se corra este metodo.
+     * IMPORTANTE SIEMPRE PONER @FXML
+     */
+    @FXML
+    private void setPasswordField() {
+        String s = userField.getText();
+        passwordField.setText(s);
+        loginBtn.setDisable(true);
+    }
 }
--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         //Cambiar a Login.fxml para probar metodos o a Main.fxml para reproductor
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         primaryStage.setTitle("Odyssey++");
         primaryStage.setScene(new Scene(root, 1040, 650));
         primaryStage.setResizable(false);
