a34da13804c5737f95f47749939a2697fb901a81
Róger Valderrama <roger.andres18@gmail.com>
2018-05-27 15:33:49 -0600

    Fix Dialog

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -32,17 +32,11 @@ public class Main extends Application {
     }
     
     public static void exitServer () {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Confirmation Dialog");
-        alert.setHeaderText("Look, a Confirmation Dialog");
-        alert.setContentText("Are you ok with this?");
-        
-        Optional<ButtonType> result = alert.showAndWait();
-        if (result.get() == ButtonType.OK) {
-            Platform.exit();
-        } else {
-            alert.close();
-        }
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Information Dialog");
+        alert.setHeaderText(null);
+        alert.setContentText("Request Server Shutdown");
+        alert.showAndWait();
         
     }
     
